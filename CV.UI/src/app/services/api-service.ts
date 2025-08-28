5b72b93a3797d5701d7a8d6657e2248f651952a6
Connor <connor.morton.95@gmail.com>
2025-08-28 17:07:45 +0100

    Tweaks

--- CV.UI/src/app/services/api-service.ts
+++ CV.UI/src/app/services/api-service.ts
@@ -10,7 +10,7 @@ import { Hobby } from '../models/hobby';
 })
 export class ApiService {
   getIntroduction(): Observable<string> {
-    const introduction: string = "Hi, I'm Connor. I just joined the 30's club and have been a technology enthusiast and professional software developer for most of those years. " + 
+    const introduction: string = "Hi, I'm Connor. I just joined the 30's club and have been a technology enthusiast and software developer for most of those years. " + 
     "I began hobbyist software development as a teenager, with roots in Java, SQL and C(++). Since then, I've graduated university with a 2-1 " +
     "and moved on to gain 10 years of professional software development experience, primarily in the .NET stack and cloud technologies. I " +
     "perform best as a core member of a team, and love to help and mentor others whilst also learning alongside them. "; //TODO replace with API call
