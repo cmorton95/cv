18a263a5ad31334d06f988bcd892ccb3b69e16a9
Connor <connor.morton.95@gmail.com>
2025-08-29 11:05:14 +0100

    Introduction Fixes

--- CV.API/CV.API/Program.cs
+++ CV.API/CV.API/Program.cs
@@ -25,6 +25,12 @@ app.UseAuthorization();
 app.UseCors(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 app.MapEndpoints();
+app.MapGet("intro", () => Results.Ok("Hi, I'm Connor. I just joined the 30's club and have been a technology " +
+    "enthusiast and software developer for most of those years. I began hobbyist software development as " +
+    "a teenager, with roots in Java, SQL and C(++). Since then, I've graduated university with a 2-1 " +
+    "and moved on to gain 10 years of professional software development experience, primarily in the " +
+    ".NET stack and cloud technologies. I perform best as a core member of a team, and love to help " +
+    "and mentor others whilst also learning alongside them. "));
 
 app.UseSwagger();
 app.UseSwaggerUI();
--- CV.UI/src/app/services/api-service.ts
+++ CV.UI/src/app/services/api-service.ts
@@ -4,38 +4,44 @@ import { WorkExperience } from '../models/work-experience';
 import { Skill } from '../models/skill';
 import { Qualification } from '../models/qualification';
 import { Hobby } from '../models/hobby';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  apiUri: string = "https://localhost:7248";
+  apiBase: URL = new URL("https://localhost:7248");
 
   constructor(private client: HttpClient) {}
 
   getIntroduction(): Observable<string> {
-    const introduction: string = "Hi, I'm Connor. I just joined the 30's club and have been a technology enthusiast and software developer for most of those years. " + 
-    "I began hobbyist software development as a teenager, with roots in Java, SQL and C(++). Since then, I've graduated university with a 2-1 " +
-    "and moved on to gain 10 years of professional software development experience, primarily in the .NET stack and cloud technologies. I " +
-    "perform best as a core member of a team, and love to help and mentor others whilst also learning alongside them. "; //TODO replace with API call
-    return of(introduction);
+    const url = new URL('intro', this.apiBase);
+    return this.client.get<string>(url.toString());
   }
 
   getWorkExperience(): Observable<WorkExperience[]> {
-    return this.client.get<WorkExperience[]>(`${this.apiUri}/work`);
+    const url = new URL('work', this.apiBase);
+    return this.client.get<WorkExperience[]>(url.toString());
   }
 
   getSkills(page: number): Observable<Skill[]> {
     const pageSize = 6;
-    return this.client.get<Skill[]>(`${this.apiUri}/skill?page=${page}&pageSize=${pageSize}`);
+    const url = new URL('skill', this.apiBase);
+
+    const params = new HttpParams()
+      .set('page', page)
+      .set('pageSize', pageSize);
+
+    return this.client.get<Skill[]>(url.toString(), { params });
   }
 
   getQualifications(): Observable<Qualification[]> {
-    return this.client.get<Qualification[]>(`${this.apiUri}/qual`)
+    const url = new URL('qual', this.apiBase);
+    return this.client.get<Qualification[]>(url.toString());
   }
 
   getHobbies(): Observable<Hobby[]> {
-    return this.client.get<Hobby[]>(`${this.apiUri}/hobby`);
+    const url = new URL('hobby', this.apiBase);
+    return this.client.get<Hobby[]>(url.toString());
   }
 }
